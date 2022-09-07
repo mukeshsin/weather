36a44d8d9ea5389f760e960ec35e5763714c9977
mukeshsin <mukesh@innotechteam.in>
2022-09-07 23:43:56 +0530

    completed weather app

--- app.js
+++ app.js
@@ -1,35 +1,23 @@
-const iconElement = document.querySelector('.weather.icon');
-const tempElement = document.querySelector('.temperature p');
-const unitElement = document.querySelector('.unit');
-const descElement = document.querySelector('.temperature-description');
-const locationElement = document.querySelector('.location p');
-const notificationElement = document.querySelector('.notification');
-const weather = {};
-weather.temperature = {
-    unit: 'Celsius'
-
-};
-
-const kelvin = 273;
-const key = '82005d27a116c2880c8f0fcb866998a0'
-
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    }
+if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+}
+const KELVIN = 273;
 function showPosition(position) {
-    const lati = + position.coords.latitude;
-    const longi = + position.coords.longitude;
+    const lati = position.coords.latitude;
+    const longi = position.coords.longitude;
     console.log("Latitude: " + lati + ' ' + "Longitude: " + longi);
-    
 
     fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lati}&lon=${longi}&appid=82005d27a116c2880c8f0fcb866998a0`)
         .then((data) => {
             return data.json();
         })
-        then((data)=>{
-            temperature.value=Math.floor(data.main.temp-kelvin);
-            temperature
-
+        .then((data) => {
+            temperature = document.getElementById('temperature-value').innerHTML = data.main.temp;
+            celsius = temperature - KELVIN;
+            decimal = Math.floor(celsius);
+            document.getElementById('temperature-value').innerHTML = decimal;
+            document.getElementById('location').innerHTML = data.name;
+            document.getElementById('img').src = `icons/${data.weather[0].icon}.png`;
+            document.getElementById('temperature-description').innerHTML = data.weather[0].description;
         })
-    }
-
+}
